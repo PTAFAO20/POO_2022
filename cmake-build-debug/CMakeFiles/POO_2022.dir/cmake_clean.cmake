b2b26ea9c9e0d56cabd5c92e314b9fc6b179f438
InesSalgado <imcsalgado.w@gmail.com>
2022-11-08 14:36:15 +0000

    InicioDoProjeto

---
+++ cmake-build-debug/CMakeFiles/POO_2022.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POO_2022.dir/main.cpp.obj"
+  "POO_2022.exe"
+  "POO_2022.exe.manifest"
+  "POO_2022.pdb"
+  "libPOO_2022.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POO_2022.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
